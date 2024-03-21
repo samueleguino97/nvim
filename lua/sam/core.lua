33a5634cd8a2f73f086b212ac0bc9656d21b6b99
Sam Eguino <samueleguino97@gmail.com>
2024-03-21 13:08:09 -0400

    added surround and remaps

--- lua/sam/core.lua
+++ lua/sam/core.lua
@@ -9,3 +9,36 @@ vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 
 vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+
+vim.opt.smartindent = true
+
+vim.opt.wrap = false
+
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undofile = true
+vim.opt.undodir = os.getenv("HOME") .. "/.config/nvim/undodir"
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+vim.opt.termguicolors = true
+
+vim.opt.scrolloff = 8
+vim.opt.updatetime = 50
+
+vim.opt.colorcolumn = "80"
+
+vim.keymap.set("v", "J", ":m '<+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+vim.keymap.set("n", "<leader>y", '"+y')
+vim.keymap.set("v", "<leader>y", '"+y')
+vim.keymap.set("n", "<leader>Y", '"+Y')
+
+vim.keymap.set("n", "<leader>p", '"+p')
+
+vim.keymap.set("n", "Q", "<nop>")
